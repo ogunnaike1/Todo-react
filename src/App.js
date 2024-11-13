f5599513b7ab5cad9cb13eb5e9891d566186739c
ogunnaike1 <ogunnaikeusman17@gmail.com>
2024-11-13 14:34:03 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import TodoUi from './component/TodoUi';
 
 function App() {
   return (
-    <div className="bg-gray-500 text-[50px]">
-    hello
-    </div>
+    <TodoUi/>
   );
 }
 
---
+++ src/component/TodoUi.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TodoUi = () => {
+  return (
+ <h1 className='text-[100px] font-[600] text-center mt-[10vh]'>Just do it</h1>
+  )
+}
+
+export default TodoUi
