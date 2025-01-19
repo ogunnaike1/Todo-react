e0d47410d2aa74052645bb69b3a09c318c99fb26
ogunnaike1 <ogunnaikeusman17@gmail.com>
2025-01-19 23:31:57 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import { useState } from 'react';
 
 function App() {
   const [title, setTitle] = useState("");
-
+  const [content, setContent] = useState("");
+  const [showEdit, setShowEdit] = useState(true);
+  const [allTodo, setAllTodo] = useState([]);
+  const [editIndex, setEditIndex] = useState(null);
 
   let todoList = {
     title,
