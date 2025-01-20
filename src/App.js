c84056567c9afcb968ce1addb6c0cbb9a3829a00
ogunnaike1 <ogunnaikeusman17@gmail.com>
2025-01-20 23:31:40 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -15,10 +15,7 @@ function App() {
   };
 
   const handleClick = () => {
-    setAllTodo([...allTodo, todoList]);
-    setTitle("");
-    setContent("");
-  };
+
 
   const handleDelete = (index) => {
     const newTodo = allTodo.filter((item)=> item.index !== index)
