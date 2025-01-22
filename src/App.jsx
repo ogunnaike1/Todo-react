d9e5d4234a9fc5480fabb1e2ef2e27ddd937d2dd
ogunnaike1 <ogunnaikeusman17@gmail.com>
2025-01-22 23:33:57 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,10 @@ function App() {
 
   const handleClick = () => {
     const updatedTodos = [...allTodo, { title, content }];
-
+    setAllTodo(updatedTodos);
+    localStorage.setItem("localStorageTodo", JSON.stringify(updatedTodos));
+    setTitle("");
+    setContent("");
   };
 
   const handleDelete = (index) => {
