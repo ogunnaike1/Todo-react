9dd9339aa720c6e9708de1fe7e26680bd7258421
ogunnaike1 <ogunnaikeusman17@gmail.com>
2025-01-26 23:58:31 +0100

    commit

--- src/component/Display_button.jsx
+++ src/component/Display_button.jsx
@@ -15,7 +15,8 @@ const Display_button = () => {
     <div>
     <First_input handleFirstInput = {handleFirstInput}/>
     <Second_input handleSecondInput = {handleSecondInput}/>
-  
+        <p>{firstInput}</p>
+        <p>{secondInput}</p>
     </div>
   )
 }
