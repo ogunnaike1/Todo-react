445ade9d95ff0499fd57640bf84f033bd8774137
ogunnaike1 <ogunnaikeusman17@gmail.com>
2025-01-29 23:41:47 +0100

    commit

--- src/component/Todo.jsx
+++ src/component/Todo.jsx
@@ -68,8 +68,7 @@ const handleChecked = (index) => {
   setAllTodo(updatedTodos);
 
   // Filter only completed todos before saving to localStorage
-  const completedTodos = updatedTodos.filter(todo => todo.completed);
-  localStorage.setItem("localStorageTodo", JSON.stringify(completedTodos));
+
 };
 
  console.log(allTodo);
